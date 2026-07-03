f157a3714e9046b811f02b742d5c7f60be2f7a87
Sanidhya Ajudiya <your_email@example.com>
2026-07-03 11:03:22 +0530

    Updated project files

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://crossmart.onrender.com/api';
 
 export interface ApiFetchOptions extends RequestInit {
   bodyData?: any;
